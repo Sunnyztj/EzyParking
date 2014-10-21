d70e276bb4186c518910479dfdf95ce44a275b0d
sunny <webztj@gmail.com>
2014-10-21 20:27:45 +1030

    Initial Commit

---
+++ EzyParking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EzyParking
+//
+//  Created by Ben Zhang on 10/21/14.
+//  Copyright (c) 2014 Sunny Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EzyParking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EzyParking
+//
+//  Created by Ben Zhang on 10/21/14.
+//  Copyright (c) 2014 Sunny Zhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
